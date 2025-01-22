a0dd93f56fc563735487e4c02524a2ef6d22abc0
ehdrjs4802 <ehdrjs4802@gmail.com>
2025-01-22 16:03:34 +0900

    2-9 ãSpringSecurity (fix:bug)

--- src/main/java/org/example/expert/config/FilterConfig.java
+++ src/main/java/org/example/expert/config/FilterConfig.java
@@ -1,22 +1,21 @@
-package org.example.expert.config;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.boot.web.servlet.FilterRegistrationBean;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@RequiredArgsConstructor
-public class FilterConfig {
-
-    private final JwtUtil jwtUtil;
-
-    @Bean
-    public FilterRegistrationBean<JwtFilter> jwtFilter() {
-        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
-        registrationBean.setFilter(new JwtFilter(jwtUtil));
-        registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
-
-        return registrationBean;
-    }
-}
+//package org.example.expert.config;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.boot.web.servlet.FilterRegistrationBean;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//@Configuration
+//@RequiredArgsConstructor
+//public class FilterConfig {
+//
+//    private final JwtUtil jwtUtil;
+//    // 학습 참고용으로 삭제 하지 않았습니다.
+//    public FilterRegistrationBean<JwtFilter> jwtFilter() {
+//        FilterRegistrationBean<JwtFilter> registrationBean = new FilterRegistrationBean<>();
+//        registrationBean.setFilter(new JwtFilter(jwtUtil));
+//        registrationBean.addUrlPatterns("/*"); // 필터를 적용할 URL 패턴을 지정합니다.
+//
+//        return registrationBean;
+//    }
+//}
